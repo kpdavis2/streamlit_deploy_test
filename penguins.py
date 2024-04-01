20bcd59a5e72c2cf6340ea7ffd77647b8caf28e7
kpdavis2 <kpd2001@charter.net>
2024-04-01 19:15:43 -0400

    update penguins

--- penguins.py
+++ penguins.py
@@ -2,28 +2,49 @@ import streamlit as st
 import pandas as pd
 import altair as alt
 import seaborn as sns
+import time
+
 st.title("Palmer's Penguins")
+
 st.markdown('Use this Streamlit app to make your own scatterplot about penguins!')
 penguin_file = st.file_uploader(
-'Select Your Local Penguins CSV (default provided)')
-if penguin_file is not None:
-    penguins_df = pd.read_csv(penguin_file)
-else:
-    penguins_df = pd.read_csv('penguins.csv')
+    'Select Your Local Penguins CSV (default provided)')
+
+@st.cache_data()
+def load_file(penguin_file):
+    time.sleep(5)
+    if penguin_file is not None:
+        penguins_df = pd.read_csv(penguin_file)
+    else:
+        penguins_df = pd.read_csv('penguins.csv')
+    return(penguins_df)
+
+penguins_df = load_file(penguin_file)
+
+#if penguin_file is not None:
+#    penguins_df = pd.read_csv(penguin_file)
+#else:
+ #   penguins_df = pd.read_csv('penguins.csv')
+
+
 selected_x_var = st.selectbox('What do you want the x variable to be?',
     ['bill_length_mm', 'bill_depth_mm',
     'flipper_length_mm', 'body_mass_g'])
+
 selected_y_var = st.selectbox('What about the y?',
     ['bill_depth_mm', 'bill_length_mm',
     'flipper_length_mm', 'body_mass_g'])
+
 selected_gender = st.selectbox('What gender do you want to filter for?',
     ['all penguins', 'male penguins', 'female penguins'])
+
 if selected_gender == 'male penguins':
     penguins_df = penguins_df[penguins_df['sex'] == 'male']
 elif selected_gender == 'female penguins':
     penguins_df = penguins_df[penguins_df['sex'] == 'female']
 else:
     pass
+
 alt_chart = (
     alt.Chart(penguins_df, title="Scatterplot of Palmer's Penguins")
         .mark_circle()
@@ -34,4 +55,5 @@ alt_chart = (
         )
         .interactive()
     )
+
 st.altair_chart(alt_chart, use_container_width=True)
